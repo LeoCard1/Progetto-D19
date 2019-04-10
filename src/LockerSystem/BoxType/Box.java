b101056145ed3a3ad5ab4a6d3bb5270149b0e99c
Andrea3797 <andrea.stella02@universitadipavia.it>
2019-04-10 15:23:19 +0200

    creata classe box e sottoclassi

--- src/LockerSystem/BoxType/Box.java
+++ src/LockerSystem/BoxType/Box.java
@@ -1,7 +1,29 @@
 package LockerSystem.BoxType;
 
+import LockerSystem.Size;
+import LockerSystem.Package;
+import java.util.Date;
+
 public abstract class Box {
 
+    protected boolean availability;
+    protected Size size;
+    private Package pacco;
+    protected static int code;
+    protected Date date;
 
+    public Box(){
+        availability = true;
+        code++;
+    }
+
+    public void addPackage(Package pacco){
+        this.pacco = pacco;
+        availability = false;
+    }
+
+    public void removePackage(){
+         availability = true;
+    }
 
 }
