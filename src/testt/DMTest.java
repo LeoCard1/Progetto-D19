e3376e27d76f2a1d9845e6a62142cdb8e1384261
LeoCard1 <leonardo.cardone01@universitadipavia.it>
2019-04-10 17:23:22 +0200

    added Unit Test

---
+++ src/testt/DMTest.java
@@ -0,0 +1,21 @@
+import LockerSystem.DeliveryMan;
+import LockerSystem.Package;
+import LockerSystem.Size;
+import org.junit.jupiter.api.Test;
+
+public class DMTest {
+    DeliveryMan deliveryMan = new DeliveryMan("ciao");
+    Package aPackage = new Package("pacco1", new Size(5,5,5));
+
+    @Test
+    public void addPackageTest() {
+        deliveryMan.addPackage(aPackage);
+    }
+
+    @Test
+    public void removePackageTest() {
+        deliveryMan.removePackage(aPackage);
+    }
+
+
+}
