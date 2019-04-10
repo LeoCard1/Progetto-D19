5a78b3f6782e4479557738d52ab258c24458432a
ZappaRoberto <roberto.zappa01@universitadipavia.it>
2019-04-10 13:52:57 +0200

    Prima versione classe DeliveryMan

--- src/LockerSystem/DeliveryMan.java
+++ src/LockerSystem/DeliveryMan.java
@@ -1,4 +1,20 @@
 package LockerSystem;
 
+import java.util.ArrayList;
+
 public class DeliveryMan {
+    private String id; // id univoco che identifica un fattorino
+    private ArrayList<Package>listaPacchi;
+
+    DeliveryMan(String id){
+        this.id = id;
+    }
+
+    public void addPackage(Package pacco){
+        listaPacchi.add(pacco);
+    }
+
+    public void removePackage(Package pacco){
+        listaPacchi.remove(pacco);
+    }
 }
