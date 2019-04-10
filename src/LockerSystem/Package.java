ac66fad0812c410fb914248ab5e9683d220126ad
ZappaRoberto <roberto.zappa01@universitadipavia.it>
2019-04-10 13:44:23 +0200

    Classe Package

--- src/LockerSystem/Package.java
+++ src/LockerSystem/Package.java
@@ -1,4 +1,10 @@
 package LockerSystem;
 
 public class Package {
+    private String id; // Codice identificativo di un pacco
+    private Size dimension; // Dimensione del pacco
+    Package(String id , Size dimension){
+        this.dimension = dimension;
+        this.id = id;
+    }
 }
