41d1f9be5e9713588668000b4afd15a6e1b3af4f
ZappaRoberto <roberto.zappa01@universitadipavia.it>
2019-04-11 15:00:38 +0200

    aggiunti dati statistici classe deliveryman e corrette le nomenclature di alcune variabili

--- src/LockerSystem/DeliveryMan.java
+++ src/LockerSystem/DeliveryMan.java
@@ -5,17 +5,20 @@ import java.util.ArrayList;
 public class DeliveryMan {
     private String id; // id univoco che identifica un fattorino
     private ArrayList<Package> listaPacchi;
+    private int packCounter; // pacchi totali consegnati. Utilizzo nei dati statistici
 
     public DeliveryMan(String id){
         this.id = id;
         listaPacchi = new ArrayList<>();
+        packCounter = 0;
     }
 
-    public void addPackage(Package pacco){
-        listaPacchi.add(pacco);
+    public void addPackage(Package pack){
+        listaPacchi.add(pack);
+        packCounter++;
     }
 
-    public void removePackage(Package pacco){
-        listaPacchi.remove(pacco);
+    public void removePackage(Package pack){
+        listaPacchi.remove(pack);
     }
 }
