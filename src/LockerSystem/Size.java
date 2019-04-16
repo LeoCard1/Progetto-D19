5927e0c879938c31479743d66107b01e55f0db11
Gentilini Sergio <sergio.gentilini01@universitadipavia.it>
2019-04-16 16:24:31 +0200

    Aggiunto il compareTo a Size.

--- src/LockerSystem/Size.java
+++ src/LockerSystem/Size.java
@@ -1,14 +1,37 @@
 package LockerSystem;
 
-public class Size {
+public class Size implements Comparable {
 
     private double height;
-    private double lenght;
+    private double length;
     private double width;
 
-    public Size(double height, double lenght, double width ) {
+    public Size(double height, double length, double width ) {
         this.height = height;
-        this.lenght = lenght;
+        this.length = length;
         this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Size sizeObject = (Size)o;
+
+        if ((height == sizeObject.height) && (length == sizeObject.getLength()) && (width == sizeObject.getWidth())) return 0;
+
+        if ((height >= sizeObject.height) && (length >= sizeObject.getLength()) && (width >= sizeObject.getWidth())) return 1;
+
+        return -1;
     }
 }
