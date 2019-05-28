9cf955ec662fae64b117a938d88edfb4f8065acb
Gentilini Sergio <sergio.gentilini01@universitadipavia.it>
2019-05-28 16:00:52 +0200

    Aggiunti commenti ai metodi di ViewBoxesPanel.

--- src/PickupPointSystem/GraphicalInterface/ViewBoxesPanel.java
+++ src/PickupPointSystem/GraphicalInterface/ViewBoxesPanel.java
@@ -6,9 +6,19 @@ import ObserverPattern.Observer;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * @author Sergio Gentilini
+ * @version 1.0.1
+ */
+
 public class ViewBoxesPanel extends JPanel implements Observer {
     private PickupPoint piPo;
     private GridBoxesPanel gridBoxesPanel;
+
+    /**
+     * The constructor.
+     * @param pipo The pickup point.
+     */
 
     public ViewBoxesPanel(PickupPoint pipo) {
         piPo = pipo;
@@ -17,11 +27,19 @@ public class ViewBoxesPanel extends JPanel implements Observer {
         initPanel();
     }
 
+    /**
+     * This method initialises the panel.
+     */
+
     private void initPanel() {
         gridBoxesPanel = new GridBoxesPanel(piPo);
         GridBoxesPanel grid = gridBoxesPanel;
         add(grid);
     }
+
+    /**
+     * The interface is updated after a package is added or removed.
+     */
 
     @Override
     public void update() {
