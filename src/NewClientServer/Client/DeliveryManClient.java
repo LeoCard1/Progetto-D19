5b43372b86cedbf2f08c2cd9779fbc9cc5995275
Sergio Gentilini <sergio.gentilini01@universitadipavia.it>
2019-05-31 13:46:52 +0200

    Definito prototipo di client per il DeliveryMan in NewClientServer.

---
+++ src/NewClientServer/Client/DeliveryManClient.java
@@ -0,0 +1,45 @@
+package NewClientServer.Client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class DeliveryManClient {
+    private Socket client;
+    private BufferedReader in;
+    private PrintStream out;
+
+    public void connectToPickupPoint() {
+        try {
+            client = new Socket();
+            client.connect(new InetSocketAddress("localhost", 8000));
+
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintStream(client.getOutputStream(), true);
+
+            goConnect();
+
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.err.println("Error: can't connect to the pickup point.");
+        }
+    }
+
+    private void goConnect() throws IOException {
+        System.out.println("Connection estabilished!");
+
+        while (true) {
+            out.println("deliveryman");
+            out.println("Prova");
+            out.println("close");
+
+            in.close();
+            out.close();
+
+            client.close();
+        }
+    }
+}
--- src/NewClientServer/Main.java
+++ src/NewClientServer/Main.java
@@ -1,10 +1,14 @@
 package NewClientServer;
 
+import NewClientServer.Client.DeliveryManClient;
 import NewClientServer.Server.PickupPointServer;
 
 public class Main {
     public static void main(String[] args) {
         Thread server = new PickupPointServer();
         server.start();
+
+        DeliveryManClient del = new DeliveryManClient();
+        del.connectToPickupPoint();
     }
 }
