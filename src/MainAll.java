c37d50dcc92989a45d2614fdc95456f549f25ff0
Gentilini Sergio <sergio.gentilini01@universitadipavia.it>
2019-07-19 17:09:19 +0200

    Aggiunto MainAll.

---
+++ src/MainAll.java
@@ -0,0 +1,14 @@
+import DeliveryManSystem.GraphicalInterfaceClientSystem.Gui;
+import PickupPointSystem.GraphicalInterface.GraIntMain;
+import ServerAndDatabase.MainServer;
+
+public class MainAll {
+    public static void main(String[] args) {
+        MainServer server = new MainServer();
+        server.start();
+
+        new GraIntMain("PAV01");
+
+        new Gui();
+    }
+}
