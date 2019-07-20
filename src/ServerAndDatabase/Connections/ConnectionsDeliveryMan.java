7a742367e9289c07e0e28e10fbfb88b7e1316151
Sergio Gentilini <sergio.gentilini01@universitadipavia.it>
2019-07-20 14:45:34 +0200

    Aggiunti commenti javadoc alle classi di ServerAndDatabase/Connections.

--- src/ServerAndDatabase/Connections/ConnectionsDeliveryMan.java
+++ src/ServerAndDatabase/Connections/ConnectionsDeliveryMan.java
@@ -8,9 +8,29 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.StringTokenizer;
 
+/**
+ * This class manages the requests
+ * pertaining to the 'deliverymen' table
+ *
+ * @author Gruppo D19
+ * @version 1.0.0
+ */
+
 public class ConnectionsDeliveryMan implements Connection {
     private DatabaseConnector datCon = new DatabaseConnector();
     private Statement statement = datCon.connectAndReturnStatement();
+
+    /**
+     * This method performs a specific task
+     * on the database depending on the
+     * string received by the server.
+     * It performs queries on the 'deliverymen'
+     * table
+     *
+     * @param out Server output
+     * @param strTok String received by the server
+     * @throws SQLException Input/Output error between client and server
+     */
 
     @Override
     public void manageConnection(PrintStream out, StringTokenizer strTok) throws SQLException {
@@ -24,6 +44,16 @@ public class ConnectionsDeliveryMan implements Connection {
             get(strTok, out);
         }
     }
+
+    /**
+     * This method returns the password of the
+     * specified deliveryman.
+     * It's used to confirm/deny authentications
+     *
+     * @param strTok String received by the server where the deliveryman ID is specified
+     * @param out Server output
+     * @throws SQLException Input/Output error between client and server
+     */
 
     private void get(StringTokenizer strTok, PrintStream out) throws SQLException {
         if (!strTok.hasMoreTokens()) {
--- src/ServerAndDatabase/Connections/ConnectionsFactory.java
+++ src/ServerAndDatabase/Connections/ConnectionsFactory.java
@@ -1,11 +1,28 @@
 package ServerAndDatabase.Connections;
 
+/**
+ * This class returns a 'Connections'
+ * class depending on the string received
+ * by the server
+ *
+ * @author Gruppo D19
+ * @version 1.0.1
+ */
+
 public class ConnectionsFactory {
 
     private ConnectionsDeliveryMan connectionsDeliveryMan = new ConnectionsDeliveryMan();
     private ConnectionsDelivery connectionsDelivery = new ConnectionsDelivery();
     private ConnectionsPackage connectionsPackage = new ConnectionsPackage();
     private ConnectionsPickupPoint connectionsPickupPoint = new ConnectionsPickupPoint();
+
+    /**
+     * This method returns a 'Connections'
+     * class depending on the specified type
+     *
+     * @param type The type of 'Connections' class to return
+     * @return The class that manages the connection
+     */
 
     public Connection getConnection(String type) {
         if (type.equals("deliveryman")) return connectionsDeliveryMan;
